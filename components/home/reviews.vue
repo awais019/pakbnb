24935aee54a378f5d58e9c4a5b2d8f8196b4283d
Muhammad Awais <awaistopper20@gmail.com>
2023-01-02 10:37:07 +0500

    Some style changes in reviews section.

--- components/home/reviews.vue
+++ components/home/reviews.vue
@@ -5,9 +5,13 @@
       :key="index"
       class="grid grid-cols-2 mb-6 align-middle"
     >
-      <div class="flex flex-col justify-between items-center border-secondary-light border-l-2">
-        <img :src="review.reviewer.image" class="rounded-full" />
-        <p class="font-bold">{{ review.reviewer.name }}</p>
+      <div
+        class="border-secondary-light border-l-2 px-8"
+      >
+        <div class="flex flex-col justify-center items-center gap-2">
+          <img :src="review.reviewer.image" class="rounded-full" />
+          <p class="font-bold">{{ review.reviewer.name }}</p>
+        </div>
       </div>
       <div class="flex flex-col bg-secondary-light p-5 rounded-lg">
         <div class="flex justify-between py-2">
@@ -16,7 +20,11 @@
             <icons-star v-for="i in review.rating" :key="i" />
           </div>
         </div>
-        <home-short-text class="text-light" :text="review.comment" :target="150" />
+        <home-short-text
+          class="text-light"
+          :text="review.comment"
+          :target="150"
+        />
       </div>
     </div>
   </div>
