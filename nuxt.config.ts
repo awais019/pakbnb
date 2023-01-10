db24243dfe57ae12bf0aaf036126ed06a5aa7e4f
Muhammad Awais <awaistopper20@gmail.com>
2023-01-10 12:26:41 +0500

    Installed firebase and added plugin.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,11 +21,20 @@ export default defineNuxtConfig({
     "~/plugins/homes.ts",
     "~/plugins/reviews.ts",
     "~/plugins/users.ts",
+    "~/plugins/firebase.client.ts",
   ],
   runtimeConfig: {
     public: {
       mapbox: {
         accessToken: process.env.MAPBOX_ACCESS_TOKEN,
+      },
+      firebase: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
       },
     },
   },
